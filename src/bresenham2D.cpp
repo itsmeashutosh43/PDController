61cc030d867f94950c99564762e26978167d89b8
Ashutosh <chapagainashutosh8@gmail.com>
2020-03-11 14:45:57 +0545

    code cleaned

--- src/bresenham2D.cpp
+++ src/bresenham2D.cpp
@@ -17,78 +17,10 @@ bresenham2D::bresenham2D(double length_)
   costmap_subscriber = nh_.subscribe("/move_base/local_costmap/costmap",1,&bresenham2D::costmapCallback,this);  
   amcl_pose = nh_.subscribe("/amcl_pose", 1 , &bresenham2D::amcl_callback, this);
 
-  ROS_INFO("Waiting for costmap to come up.");
   length = &length_;
-
 
   refresh_data();
   refresh_data_filter();
-
-  //controller_costmap_ros_ = controller_costmap_ros_;
-
-  ros::Duration(2).sleep();
-}
-
-
-bool bresenham2D::convolve()
-{
-    
-
-
-    cv::Mat kernel = cv::Mat(40,40, CV_32F, &filter);
-    cv::Mat src = cv::Mat((int)(80) ,(int)(80) , CV_32F, &data);
-
-    
-    cv::filter2D(src,dest,-1,kernel);
-    cv::imshow("_show_",dest);
-    cv::waitKey(1);
-
-    dest = dest.reshape(1, dest.rows * dest.cols);
-
-    cv::Mat costmap_data_mat = cv::Mat(6400,1,CV_32F,&costmap_data);
-    
-    double dot_sum = dest.dot(costmap_data_mat);
-
-    if (dot_sum > 10)
-    {
-        ROS_INFO("Dot product is %f", dot_sum);
-        return true;
-    }
-
-    return false;
-
-    
-}
-
-
-int bresenham2D::compute(int *x, double goalX,double goalY)
-{
-
-    
-    if (m.try_lock())
-    {
-        refresh_data();
-        find_line(curr_pose_x,curr_pose_y,goalX,goalY);
-        
-    if (convolve())
-    {
-        *x = 1;
-        //refresh_data();
-        m.unlock();
-        return 0;
-    }
-    
-    }
-
-    else{
-            ROS_ERROR("lock not recieved");
-        }
-
-    m.unlock();
-
-    *x = 0;
-
-
 }
 
 
@@ -100,6 +32,39 @@ void bresenham2D::amcl_callback(const geometry_msgs::PoseWithCovarianceStampedCo
     curr_pose_y = pose->pose.pose.position.y;
 
 }
+
+
+
+
+void bresenham2D::costmapCallback(const nav_msgs::OccupancyGrid::ConstPtr& costmap )
+{
+
+    if (m.try_lock())
+    {
+
+        height = costmap->info.height;
+        width = costmap->info.width;
+        resolution_ = costmap->info.resolution;  
+        originX_ = costmap->info.origin.position.x;
+        originY_ = costmap->info.origin.position.y;
+            
+        *costmap_ = true;
+    
+        for (auto i = 0 ; i< (height*width) ; i++)
+        {
+                costmap_data[i] = costmap->data[i];
+        }
+        m.unlock();
+    }
+        
+        
+    else{
+            ROS_ERROR("lock not recieved");
+    }
+
+}
+
+
 
 bool bresenham2D::check_robot_path(double goalX, double goalY)
 {
@@ -123,6 +88,66 @@ bool bresenham2D::check_robot_path(double goalX, double goalY)
 }
 
 
+int bresenham2D::compute(int *x, double goalX,double goalY)
+{
+
+    
+    if (m.try_lock())
+    {
+        refresh_data();
+        find_line(curr_pose_x,curr_pose_y,goalX,goalY);
+        
+    if (convolve())
+    {
+        *x = 1;
+        m.unlock();
+        return 0;
+    }
+    
+    }
+
+    else{
+            ROS_ERROR("lock not recieved");
+        }
+
+    m.unlock();
+
+    *x = 0;
+
+
+}
+
+
+
+bool bresenham2D::convolve()
+{
+    
+
+
+    cv::Mat kernel = cv::Mat(40,40, CV_32F, &filter);
+    cv::Mat src = cv::Mat((int)(80) ,(int)(80) , CV_32F, &data);
+
+    
+    cv::filter2D(src,dest,-1,kernel);
+    
+
+    dest = dest.reshape(1, dest.rows * dest.cols);
+
+    cv::Mat costmap_data_mat = cv::Mat(6400,1,CV_32F,&costmap_data);
+    
+    double dot_sum = dest.dot(costmap_data_mat);
+
+    if (dot_sum > 10)
+    {
+        ROS_INFO("Dot product is %f", dot_sum);
+        return true;
+    }
+
+    return false;
+
+    
+}
+
 
 void bresenham2D::find_line( double x,double y,double x2, double y2){
 
@@ -143,7 +168,6 @@ void bresenham2D::find_line( double x,double y,double x2, double y2){
     }
     int numerator = longest >> 1 ;
     for (int i=0;i<=longest;i++) {
-        //putpixel(x,y,color) ;
 
         put_point(x,y);
         numerator += shortest ;
@@ -158,10 +182,6 @@ void bresenham2D::find_line( double x,double y,double x2, double y2){
     }
  
 }
- 
-
-
-
 
 void bresenham2D::put_point(double x ,double y)
 {
@@ -217,97 +237,20 @@ void bresenham2D::refresh_data_filter()
 }
 
 
-bool bresenham2D::dot_product(cv::Mat* output)
-{
-    double c = 0;
-    for (auto i = 0; i< (width*height); i++)
+bool bresenham2D::distance(int x1, int y1 , int x0 , int y0)
     {
-        //ROS_INFO("%d %d", data[i], costmap_data[i]);
-        c += (output->at<int>(i))*costmap_data[i];
-    }
 
-    if (c > 0 ){
-        
-    ROS_INFO("dot product %f" , c);
-    return true;
-    }
+        float distance = std::sqrt(std::pow(x1 - x0,2) + std::pow(y1 - y0,2));
 
-    return false;
-}
-
-
-void bresenham2D::costmapCallback(const nav_msgs::OccupancyGrid::ConstPtr& costmap )
-{
-
-        if (m.try_lock())
+        if (distance > 20)
         {
-
-
-        bool flag = false;
-        if (*costmap_count_ == 0)
-        {
-            
-            flag = true;
-            prev = ros::Time::now().toSec();
+            return false;
         }
 
-        (*costmap_count_)+=1;
+        return true;
 
-        if (true){
-            height = costmap->info.height;
-            width = costmap->info.width;
-            resolution_ = costmap->info.resolution;
-            
-            originX_ = costmap->info.origin.position.x;
-            
-            originY_ = costmap->info.origin.position.y;
-            
-            *costmap_ = true;
-
-            prev = ros::Time::now().toSec();
-        
-
-            for (auto i = 0 ; i< (height*width) ; i++)
-            {
-                costmap_data[i] = costmap->data[i];
-
-            }
-
-        }
-        m.unlock();
-        }
-
-
-        else{
-            ROS_ERROR("lock not recieved");
-        }
-
-        
-
-}
-
-    
-    
-    void bresenham2D::print_corresponding_coordinates(int x)
-    {
-        
-        int rows = x / 80 ;
-        int columns = x % 80;
-
-        double newX = *originX + *resolution*(columns);
-        double newY = *originY + *resolution* (rows);
-        ROS_INFO("%f %f", newX, newY);
     }
 
-    /*
-    
-int main (int argc, char** argv){
-    ros::init(argc,argv,"bresenham");
-    ros::NodeHandle nh;
-    ros::Subscriber goal;
-    
-    
-    bresenham2D b;
-    ros::spin();
-}
-*/
+
+
+
--- src/bresenham2D.h
+++ src/bresenham2D.h
@@ -22,75 +22,50 @@ class bresenham2D{
     public:
     ros::Subscriber costmap_subscriber, footprint , amcl_pose;
     ros::NodeHandle nh_;
+    ros::Publisher pb;
+
     int height;
-    double *yaw;
-    cv::Mat dest;
+    int width;
     int costmap_count = 0;
     int *costmap_count_ = &costmap_count;
-    ros::Publisher pb;
-    bool value = false;
-    bool *costmap_ = &(value); 
-    bool *semaphore = &(value);
-    std::mutex m;
-    bool footprint_ = false;
-    boost::recursive_mutex planner_mutex_;
-    int width;
+    int sem;
+    int *sem_ = &sem;
+    
     double PI  =3.141592653589793238463;
     double prev = 0;
-    
+    double *yaw;
+    double resolution_ = 0.025;
+    double *resolution = &(resolution_);
+    double* length;
+    double originX_;
+    double *originX = &originX_;
+    double originY_;
+    double *originY = &originY_;
+    double curr_pose_x;
+    double curr_pose_y;
+
     float data[80][80];
     float filter[40][40];
     float costmap_data[6400];
 
-    double resolution_ = 0.025;
-    double *resolution = &(resolution_);
-
-    int sem;
-    int *sem_ = &sem;
-
-    double* length;
-
-    double originX_;
-    double *originX = &originX_;
+    bool value = false;
+    bool *costmap_ = &(value); 
     
-    double originY_;
-    double *originY = &originY_;
-
-    int* offset;
-
-    double curr_pose_x;
-    double curr_pose_y;
-    bool kill =false;
-    bool* kill_p = &kill;
-    costmap_2d::Costmap2DROS *controller_costmap_ros_;
-
-    
+    cv::Mat dest;
+    std::mutex m;
+        
     bresenham2D(double length);
-    int compute(int *x,double goalX,double goalY);
-    void print_corresponding_coordinates(int x);
-    bool convolve();
+
     void amcl_callback(const geometry_msgs::PoseWithCovarianceStampedConstPtr pose);
-    void find_line(double x1, double y1, double x2, double y2);
-    void put_point(double x ,double y);
+    void costmapCallback(const nav_msgs::OccupancyGrid::ConstPtr& costmap );
+
+    bool check_robot_path(double goalX, double goalY);
     void refresh_data();
     void refresh_data_filter();
-    //void put_points(double x, double y);
-   
-    bool dot_product(cv::Mat* output);
-    //void points_inside_ellipse(double x , double y);
-    void costmapCallback(const nav_msgs::OccupancyGrid::ConstPtr& costmap );
-    bool check_robot_path(double goalX, double goalY);
-    bool distance(int x1, int y1 , int x0 , int y0)
-    {
-
-        float distance = std::sqrt(std::pow(x1 - x0,2) + std::pow(y1 - y0,2));
-
-        if (distance > 20)
-        {
-            return false;
-        }
-
-        return true;
-
-    }
+    int compute(int *x,double goalX,double goalY);
+    bool convolve();
+    void find_line(double x1, double y1, double x2, double y2);
+    void put_point(double x ,double y);
+    bool distance(int x1, int y1 , int x0 , int y0);
+    
 };
